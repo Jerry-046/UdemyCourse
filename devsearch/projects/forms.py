9663b55664f788663197fb10b3eb24ec5fe21df1
Zenesh Shrestha <dare3462@gmail.com>
2023-02-14 16:30:18 +0545

    upload

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -1,8 +1,13 @@
-from django.forms import ModelForm
+from django.forms import ModelForm,widgets
 from .models import Project
+from django import forms
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = '__all__'
+        widgets={
+            'title' : forms.TextInput(attrs={'style':'width:140%;''height:20px;','placeholder':'Enter here'}),
+            'description' : forms.TextInput(attrs={'style':'width:100%;''height:30px;','placeholder':'Enter here'}),
+        }
   
